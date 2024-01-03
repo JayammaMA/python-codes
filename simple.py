4d5d2e09f159369c3b830738bf1d38853a4a48ef
Jayamma Mahadevappa Aralikatti <146332645+JayammaMA@users.noreply.github.com>
2024-01-03 06:58:12 -0800

    Update simple.py
    generate random password

--- simple.py
+++ simple.py
@@ -10,3 +10,21 @@ password = input("Input your password: ")
 hashed_password = hash_password(password)
 print(f"Your hashed password is: {hashed_password}") 
 
+#A Python program that defines a function to generate random passwords of a specified length. The
+function takes an optional parameter length, which is set to 8 by default. If no length is specified by the
+user, the password will have 8 characters
+
+
+import random
+import string
+def generate_password(length=8):
+all_characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(all_characters) for i in range(length))
+ return password
+password_length_str = input("Input the desired length of your password:")
+if password_length_str:
+ password_length = int(password_length_str)
+else:
+ password_length = 8
+password = generate_password(password_length)
+print(f"Generated password is: {password}") 
